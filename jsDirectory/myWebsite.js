66aae11d94fcd689a7fea539cf80a20e09b85866
amai-21 <andrew.y.mai@gmail.com>
2023-11-24 14:59:30 -0600

    I've linked the css and js files to each other via relative paths. Looks like it's working!

---
+++ jsDirectory/myWebsite.js
@@ -0,0 +1,12 @@
+
+/* testing if the relative pathing worked.
+function myFunction(){
+    document.getElementById("jsTest").style.fontSize = "25px";
+
+    document.getElementById("jsTest").style.color = "red";
+
+    document.getElementById("jsTest").style.backgroundColor = "yellow";
+}
+
+
+ */
